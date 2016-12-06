d1f5755f681aea6cebcf13446fd8ba7842a3d680
eichmann <david-eichmann@uiowa.edu>
2016-12-06 19:05:28 +0000

    First fully-functional generation using new STATISTICAL scheme.  This looks like it exposes everything present in the triplestore.

--- src/main/java/edu/uiowa/slis/VIVOISF/Code/CodeIterator.java
+++ src/main/java/edu/uiowa/slis/VIVOISF/Code/CodeIterator.java
@@ -22,7 +22,7 @@ public class CodeIterator extends edu.uiowa.slis.VIVOISF.TagLibSupport {
 		currentInstance = this;
 		try {
 			rs = getResultSet(prefix+
-					" SELECT ?s ?labelUS ?labelENG, ?label ?labelANY where { "+
+					" SELECT ?s ?labelUS ?labelENG ?label ?labelANY where { "+
 					"  ?s rdf:type <http://www.w3.org/2006/vcard/ns#Code> . "+
 					"  OPTIONAL { SELECT ?labelUS  WHERE { ?s rdfs:label ?labelUS  FILTER (lang(?labelUS) = \"en-US\")}    LIMIT 1 } "+
 					"  OPTIONAL { SELECT ?labelENG WHERE { ?s rdfs:label ?labelENG FILTER (langMatches(?labelENG,\"en\"))} LIMIT 1 } "+
